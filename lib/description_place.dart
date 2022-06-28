d3098dd56afe5e2df5a8f0fb55d028c3650cab35
Yonderi Diaz Rivera <jondydiaz07@gmail.com>
2022-06-28 15:45:41 -0400

    Todo

---
+++ lib/description_place.dart
@@ -0,0 +1,89 @@
+import "package:flutter/material.dart";
+
+class DescriptionPlace extends StatelessWidget{
+
+  String namePlace;
+  int star;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.star, this.descriptionPlace);
+
+  @override
+  Widget build(BuildContext context){
+    final stars = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: const Icon(
+        Icons.star,
+        color: Colors.amber,
+        size: 25,
+      ),
+    );
+
+    final starHalf = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: const Icon(
+        Icons.star_border,
+        color: Colors.amber,
+        size: 25,
+      ),
+    );
+
+    final description = Container(
+      margin: const EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0,
+      ),
+      child: Text(descriptionPlace,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Lato',
+          color: Color(0xFF56575a),
+        ),
+      ),
+    );
+
+    final titleStars = Row(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(namePlace,
+            style: const TextStyle(
+              fontWeight: FontWeight.w900,
+              fontFamily: 'Lato',
+              fontSize: 30.0,
+            ),
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            stars,
+            stars,
+            stars,
+            stars,
+            starHalf,
+          ],
+        )
+      ],
+    );
+
+    return Column(
+      children: <Widget>[
+        titleStars,
+        description
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'description_place.dart';
+import "review_list.dart";
+import "gradient_back.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                DescriptionPlace("Duwili Ella", 5, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"),
+                ReviewList()
+              ],
+            ),
+            GradientBack(),
+          ],
+        ),
+      ),
+    );
+  }
+}
